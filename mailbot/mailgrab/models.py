0beadc59e6caa4771fc3e2e0844c3f8a668dd913
Kenneth Marsh <kenskyfish@gmail.com>
2024-12-22 13:22:53 +0100

    searches recent emails

--- mailbot/mailgrab/models.py
+++ mailbot/mailgrab/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# (venv) ken@i15-3535:~/code/myfuelai_devops/html$ grep -A 1 '^Message-ID' ExampleDispatchNotificationOrder.eml 
+# Message-ID:
+#  <SJ0PR03MB6550CA41D95E4A322D572912A9002@SJ0PR03MB6550.namprd03.prod.outlook.com>
+# (venv) ken@i15-3535:~/code/myfuelai_devops/html$ grep -m 1 '^From' ExampleDispatchNotificationOrder.eml 
+# From: MyFuelDispatch <myfueldispatch@jetagefuel.com>
+
+class EmailEML(models.Model):
+    email_message_id = models.CharField(max_length=995)
+    email_from = models.CharField(max_length=320)
+    mailbot_recieve_date = models.DateTimeField('mailbot_recieve_date')
+    mailbot_update_date = models.DateTimeField('mailbot_update_date', auto_now=True)
+    email_eml = models.TextField()
+    
--- mailbot/mailgrab/views.py
+++ mailbot/mailgrab/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
-
+import sys
 from django.http import HttpResponse
+from django.template import loader
+
+# EMAIL
+import imaplib
+import email
+import os
+
+from .models import EmailEML
 
 def index(request):
-    return HttpResponse("This is index of mailgrab app")
+    last_ten_emails = EmailEML.objects.order_by("-mailbot_update_date")[:10]
+    template = loader.get_template("index.html")
+    context = {
+        "last_ten_emails": last_ten_emails,
+    }
+    return HttpResponse(template.render(context, request))
+
+def grab(request):
+    mailgrab()
+    template = loader.get_template("grab.html")
+    return HttpResponse(template.render({}, request))
+
+def mailgrab():
+    print("Grabbing email!", file=sys.stderr)
+    mail = imaplib.IMAP4_SSL(os.environ.get("EMAIL_IMAP_SERVER"))
+    mail.login(os.environ.get("EMAIL_ADDRESS"), os.environ.get("EMAIL_PASSWORD"))
+    mail.select(os.environ.get("EMAIL_FOLDER"))
+    
+    status, data = mail.search(None, '(SINCE "20-Dec-2024")')
+
+    for item in data:
+        print(item, file=sys.stderr)
+    # f = open('kotchi.txt', 'wb')
+    # f.write(data)
+    # f.close    
