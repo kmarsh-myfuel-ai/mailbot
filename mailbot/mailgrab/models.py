afffac7529f3a0023c2783c8326e127f1f80f567
Kenneth Marsh <kenskyfish@gmail.com>
2024-12-23 17:21:06 +0100

    comment out the TextField

--- mailbot/mailgrab/models.py
+++ mailbot/mailgrab/models.py
@@ -11,5 +11,5 @@ class EmailEML(models.Model):
     email_from = models.CharField(max_length=320)
     mailbot_recieve_date = models.DateTimeField('mailbot_recieve_date')
     mailbot_update_date = models.DateTimeField('mailbot_update_date', auto_now=True)
-    email_eml = models.TextField()
+    # email_eml = models.TextField()
     
